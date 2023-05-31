ab5dd8250688dbe7ef02fc416e62404ce5ffbcab
Your Name <you@example.com>
2023-05-31 17:57:15 +0700

    LoginScreen_Case_LS1_3_5

--- src/test/java/core/BaseTest.java
+++ src/test/java/core/BaseTest.java
@@ -8,11 +8,9 @@ import org.openqa.selenium.safari.SafariDriver;
 import org.slf4j.Logger;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
-
 import java.io.File;
-
-
-
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
     private static Logger logger = LogHelper.getLogger();
@@ -21,7 +19,6 @@ public class BaseTest {
     public BaseTest() {
         keyword = new KeywordWeb();
     }
-
 
     @BeforeSuite
     public void beforeSuite() throws Exception {
@@ -32,8 +29,8 @@ public class BaseTest {
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
-
-        driver.navigate().to("https://www.glamira.co.uk/");
+//        driver.navigate().to("https://stage.glamira.co.uk/");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @AfterMethod(alwaysRun = true)
