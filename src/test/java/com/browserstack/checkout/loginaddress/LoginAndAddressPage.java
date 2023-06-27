1540f2a885b2eb52494b95448a908efb272cc27e
DungAutomation <dungntt@opentechiz.com>
2023-06-27 18:00:00 +0700

    -ADD case LoginAndAddress : LA_04,05,08,09,10

--- src/test/java/com/browserstack/checkout/loginaddress/LoginAndAddressPage.java
+++ src/test/java/com/browserstack/checkout/loginaddress/LoginAndAddressPage.java
@@ -7,6 +7,8 @@ import core.KeywordWeb;
 import core.LogHelper;
 import org.slf4j.Logger;
 
+import static core.BaseTest.jse;
+
 public class LoginAndAddressPage extends BasePage {
     private static Logger logger = LogHelper.getLogger();
     private LoginPage objLogin ;
@@ -31,6 +33,8 @@ public class LoginAndAddressPage extends BasePage {
         keyword.click("LA_BTN_LOGIN");
         keyword.untilJqueryIsDone(100L);
         keyword.assertEquals("LA_MESSAGE_INVALID_LOGIN", "LA_INPUT_MESSAGE_INVALID_LOGIN");
+        jse.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"passed\", \"reason\": \"Results found!\"}}");
+        Thread.sleep(5000);
     }
     public void clearTextAndSendKey(String clearText, String inputSendKey, String dataSendKey) throws InterruptedException {
         keyword.clearText(clearText);
@@ -42,6 +46,8 @@ public class LoginAndAddressPage extends BasePage {
         keyword.click("LA_BTN_LOGIN");
         keyword.untilJqueryIsDone(100L);
         keyword.assertEquals("LA_MESSAGE_EMAIL_INVALID", "CHECKOUT_LA_LBL_ERROR_MAIL");
+        jse.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"passed\", \"reason\": \"Results found!\"}}");
+
     }
     //Leave blank Email and Password
     public void leaveBlankEmailAndPassword() throws InterruptedException {
@@ -50,6 +56,59 @@ public class LoginAndAddressPage extends BasePage {
         keyword.click("CHECKOUT_LA_BTN_LOGIN");
         keyword.assertEquals("LA_LIST_MESSAGE_REQUIRED_FIELD", "CHECKOUT_LA_LBL_ERROR_MAIL");
         keyword.assertEquals("LA_LIST_MESSAGE_REQUIRED_FIELD", "LA_ERROR_PASSWORD");
+        jse.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"passed\", \"reason\": \"Results found!\"}}");
+
+    }
+    //Check link forgot password
+    public void checkLinkForgotPassword() throws InterruptedException {
+        keyword.click("LA_LINK_FORGOT_PASSWORD");
+        keyword.untilJqueryIsDone(50L);
+        keyword.verifyElementVisible("LP_FORGOT_PASSWORD_PAGE");
+        jse.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"passed\", \"reason\": \"Results found!\"}}");
+        Thread.sleep(5000);
+    }
+    public void sendAllDataForm(String firstName, String lastName, String email, String confirmEmail, String phone, String company, String street, String zipCode, String country) throws InterruptedException {
+        keyword.sendKeys("LP_INPUT_FIRST_NAME", firstName);
+        keyword.sendKeys("LP_INPUT_LAST_NAME", lastName);
+        keyword.sendKeys("LP_INPUT_EMAIL", email);
+        keyword.sendKeys("LP_INPUT_CONFIRM_EMAIL", confirmEmail);
+        keyword.sendKeys("LP_INPUT_TELEPHONE", phone);
+        keyword.sendKeys("LP_INPUT_COMPANY", company);
+        keyword.sendKeys("LP_INPUT_STREET", street);
+        keyword.sendKeys("LP_INPUT_POSTAL", zipCode);
+        keyword.sendKeys("LP_INPUT_CITY", country);
+    }
+    //Next to Payment page successfully with Guest option and Ship to this address is yes
+    public void nextToPaymentPageWithAddress() throws InterruptedException {
+        keyword.click("LA_BTN_BACK");
+        keyword.untilJqueryIsDone(50L);
+        keyword.click("LP_BTN_CONTINUE");
+        sendAllDataForm("LA_DATA_FIRST_NAME", "LA_DATA_LAST_NAME", "LA_DATA_EMAIL",
+                "LP_EMAIL_CONFIRM", "LA_DATA_PASSWORD", "LP_DATA_COMPANY",
+                "LP_DATA_STREET", "LP_POSTAL_CODE_MALTA", "LP_COUNTRY");
+        keyword.assertEquals("LA_MESSAGE_EMAIL_CONFIRM_ERROR", "LP_INPUT_EMAIL_CONFIRM_ERROR");
+        jse.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"passed\", \"reason\": \"Results found!\"}}");
+        Thread.sleep(5000);
+    }
+    //    emailInvalid
+    public void emailInvalid() throws InterruptedException {
+        clearTextAndSendKey("LP_INPUT_CONFIRM_EMAIL", "LP_INPUT_CONFIRM_EMAIL", "LP_EMAIL_INVALID");
+        keyword.untilJqueryIsDone(70L);
+        keyword.assertEquals("LA_MESSAGE_EMAIL_INVALID", "LP_EMAIL_CONFIRM_INVALID");
+        jse.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"passed\", \"reason\": \"Results found!\"}}");
+        Thread.sleep(5000);
+    }
+    public void checkNotCopyPasteEmail() throws InterruptedException {
+        keyword.untilJqueryIsDone(30L);
+        keyword.clearText("LP_INPUT_EMAIL");
+        keyword.clearText("LP_INPUT_CONFIRM_EMAIL");
+        keyword.untilJqueryIsDone(30L);
+        keyword.copyPaste("LP_INPUT_EMAIL", "LP_INPUT_CONFIRM_EMAIL","LP_DATA_EMAIL_COPY_PASTE");
+//        keyword.checkNotCopyPastesKeyboardEvents("LP_INPUT_EMAIL", "LP_DATA_EMAIL_COPY_PASTE");
+        keyword.untilJqueryIsDone(10L);
+        keyword.assertEquals("LA_MESSAGE_NOT_ALLOW", "LA_INPUT_MESSAGE_NOT_ALLOW_ACTION");
+        jse.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"passed\", \"reason\": \"Results found!\"}}");
+        Thread.sleep(5000);
     }
 
 
--- src/test/java/com/browserstack/checkout/loginaddress/LoginAndAddressTest.java
+++ src/test/java/com/browserstack/checkout/loginaddress/LoginAndAddressTest.java
@@ -21,15 +21,35 @@ public class LoginAndAddressTest extends BaseTest {
         objLogin.acceptAllCookies();
         objLoginAdrPage.loginCustomerWithEmailNotExistPassword();
     }
-    @Test(priority = 2, description = "Input invalid email on the Login and Address tab")
+//    @Test(priority = 2, description = "Input invalid email on the Login and Address tab")
     public void testCase_LA_03() throws InterruptedException {
         logger.info("testCase_LA_03");
         objLoginAdrPage.invalidEmailOnTheLogin();
     }
-    @Test(priority = 3, description = "Leave blank Email and Password")
+//    @Test(priority = 3, description = "Leave blank Email and Password")
     public void testCase_LA_04() throws InterruptedException {
         logger.info("testCase_LA_04");
         objLoginAdrPage.leaveBlankEmailAndPassword();
+    }
+    @Test(priority = 4, description = "Check link forgot password")
+    public void testCase_LA_05() throws InterruptedException {
+        logger.info("testCase_LA_05");
+        objLoginAdrPage.checkLinkForgotPassword();
+    }
+    @Test(priority = 5, description = "Next to Payment page successfully with Guest option and Ship to this address is yes")
+    public void testCase_LA_08() throws InterruptedException {
+        logger.info("testCase_LA_08");
+        objLoginAdrPage.nextToPaymentPageWithAddress();
+    }
+    @Test(priority = 6, description = "Input Email/Email Confirm form invalid data")
+    public void testCase_LA_09() throws InterruptedException {
+        logger.info("testCase_LA_09");
+        objLoginAdrPage.emailInvalid();
+    }
+    @Test(priority = 7, description = "Using copy/paste email for Email confirm form")
+    public void testCase_LA_10() throws InterruptedException {
+        logger.info("testCase_LA_010");
+        objLoginAdrPage.checkNotCopyPasteEmail();
     }
 
 
